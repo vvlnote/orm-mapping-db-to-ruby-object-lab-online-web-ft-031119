ace5d331591607cf10d1b4d25fbfefe1945efb42
Joy Lin <vvlnote@hotmail.com>
2019-04-20 21:29:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,16 @@ class Student
     DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade < 12
+      SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
