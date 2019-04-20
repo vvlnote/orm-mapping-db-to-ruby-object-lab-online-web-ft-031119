a2aeb7f2e3b0ae967ac4eca12b539ce2daa65fb8
Joy Lin <vvlnote@hotmail.com>
2019-04-20 21:38:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     end
   end
   
-  def self.first_students_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
         SELECT * FROM students WHERE grade = 10 LIMIT = 1
       SQL
